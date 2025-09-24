f6769dc5372aef5d34982112ac64d7d593a1f9e4
Ksksksksksksksks <mnenestrashno@gmail.com>
2025-09-24 10:28:46 +0300

    dealing with triggered but paused dags

--- airflow/dags/init_pipeline_dag.py
+++ airflow/dags/init_pipeline_dag.py
@@ -1,15 +1,21 @@
 from airflow import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+import subprocess
 
 default_args = {
     'owner': 'ksusha',
-    'start_date': datetime(2025, 9, 20),
+    'start_date': datetime.now(),
     'retry_delay': timedelta(minutes=2),
     'depends_on_past': False,
     'retries': 1,
 }
+
+def unpause_dag(dag_id):
+    subprocess.run(["airflow", "dags", "unpause", dag_id], check=True)
+
 
 with DAG(
         dag_id='init_mlops_pipeline',
@@ -21,9 +27,15 @@ with DAG(
 ) as dag:
     start = DummyOperator(task_id='start')
 
+    unpause_next = PythonOperator(
+        task_id='unpause_goemotions_preprocessing',
+        python_callable=unpause_dag,
+        op_args=['goemotions_preprocessing'],
+    )
+
     trigger_data_processing = TriggerDagRunOperator(
         task_id='trigger_data_processing',
         trigger_dag_id='goemotions_preprocessing',
     )
 
-    start >> trigger_data_processing
+    start >> unpause_next>> trigger_data_processing
